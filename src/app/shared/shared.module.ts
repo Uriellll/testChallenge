5a5211f632eb9c3f07fa95e471fd3de5d9f93def
Uriel Dávila <uri.davi.14@gmail.com>
2023-01-16 18:51:46 -0600

    6.Create shared components

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,22 @@
+import { AppRoutingModule } from './../app-routing.module';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+
+
+
+@NgModule({
+  declarations: [
+    HeaderComponent,
+    FooterComponent
+  ],
+  exports:[
+    HeaderComponent,
+  FooterComponent],
+  imports: [
+    CommonModule,
+    AppRoutingModule
+  ]
+})
+export class SharedModule { }
